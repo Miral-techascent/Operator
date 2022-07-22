1d3337d07ac616c599b8c76cc769ecfa56b8b513
Miral Patel <patelmv.2995@gmail.com>
2022-07-22 22:41:01 +0100

    operator

---
+++ operator.java
@@ -0,0 +1,24 @@
+package Operator;
+
+public class operator {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+
+        System.out.println("Addition= "+(a+b));
+        System.out.println("Subtraction= "+(a-b));
+        System.out.println("Multiplication= "+(a*b));
+        System.out.println("Division= "+(b/a));
+        System.out.println("Module= "+(a%b));
+        System.out.println("Increment Operator");
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(++b);
+        System.out.println(b++);
+        System.out.println("Decrement Operator");
+        System.out.println(--a);
+        System.out.println(a--);
+        System.out.println(b--);
+        System.out.println(--b);
+    }
+}
